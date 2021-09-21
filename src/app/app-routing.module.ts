ee0f056ec51107c689a47d57a2990ce47c7d7574
Matt Lundberg <matthew.d.lundberg@gmail.com>
2021-09-20 20:56:28 -0600

    Routing Setup
    Adding Modules and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'contact', 
+    loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule) 
+  },
+  { 
+    path: 'home', 
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) 
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
